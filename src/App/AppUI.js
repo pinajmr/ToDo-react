4a74bc3f36d88ac932b33d84286e95afae0890e4
pinajmr <pinajoma0@gmail.com>
2022-07-07 17:12:46 -0400

    Manejo de Efectos

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,7 +5,10 @@ import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 
+// Desesctructuramos las nuevas props
 function AppUI({
+    loading,
+    error,
     totalTodos,
     completedTodos,
     searchValue,
@@ -26,6 +29,12 @@ function AppUI({
         />
   
         <TodoList>
+          {/* Show message for error */}
+          {error && <p>Oww There are an error!</p>}
+          {/* Show message for loading data */}
+          {loading && <p> We are loading this site, keep calm</p>}
+          {/* Finish load and they have not exist TODOS */}
+          {(!loading && !searchedTodos.length) && <p> Create your first TODO!</p>}
           {searchedTodos.map(todo => (
             <TodoItem
               key={todo.text}
--- src/App/index.js
+++ src/App/index.js
@@ -9,33 +9,65 @@ import { AppUI } from './AppUI';
 // ];
 
 function useLocalStorage(itemName, intialValue){
-  //Traemos nuestros TODOs almacenados
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-
-  if (!localStorageItem) {
-    // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacio
-    localStorage.setItem(itemName,JSON.stringify(intialValue));
-    parsedItem = intialValue;
-  } else {
-    // Si exsiten TODOs en el localStorage los regresamos como nuestros todos
-    parsedItem = JSON.parse(localStorageItem);
-  }
-
+  // Creamos el estado inicial para nuestros errores y carga
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
   // Guardamos nuestros TODOS del localStorage en nuestro estado
-  const [item, setItem] = React.useState(parsedItem);
+  const [item, setItem] = React.useState(intialValue);
 
+  React.useEffect(() => {
+    //Simulamos un segundo delay de carga
+    setTimeout(() => {
+      // Manejamos la tara dentro de un try/catch por si ocurre algun error
+      try {
+        //Traemos nuestros TODOs almacenados
+          const localStorageItem = localStorage.getItem(itemName);
+          let parsedItem;
+
+          if (!localStorageItem) {
+            // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacio
+            localStorage.setItem(itemName,JSON.stringify(intialValue));
+            parsedItem = intialValue;
+          } else {
+            // Si exsiten TODOs en el localStorage los regresamos como nuestros todos
+            parsedItem = JSON.parse(localStorageItem);
+          }
+            setItem(parsedItem);
+          } catch(error) {
+            // Caso de un error lo guardamos en el estado
+            setError(error);
+          } finally {
+            // Tambien podemos utilizar la ultima parte finally para terminar la carga
+            setLoading(false);
+          }
+    }, 1000);
+  });
+
+
+  
+
+ 
   //Creamos la funcion en la que actualizaremos nuestro localStorage
   const saveItem = (newItem) => {
-    // convertimos a string nuestros TODOs
-    const stringifiedItem = JSON.stringify(newItem);
-    // Los guardamos en el localStorage
-    localStorage.setItem(itemName,stringifiedItem);
-    //Actualizamos nuestro estado
-    setItem(newItem);
-  }
+    try {
+      // convertimos a string nuestros TODOs
+      const stringifiedItem = JSON.stringify(newItem);
+      // Los guardamos en el localStorage
+      localStorage.setItem(itemName,stringifiedItem);
+      //Actualizamos nuestro estado
+      setItem(newItem);
+    } catch(error) {
+      setError(error);
+    }
+  };
 
-  return [item, saveItem];
+  // Para tener un mejor control de los datos retornados, podemos regresarlos dentro de un objeto
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
 }
 
 
@@ -43,7 +75,12 @@ function useLocalStorage(itemName, intialValue){
 function App() {
   //Desestructuramos los datos que retornamos de nuestro custom hook
   // y le pasamos los argumentos que necesitamos (nombre y estaod inicial)
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1',[])
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1',[])
   // Cantidad de TODOs completados
   const completedTodos = todos.filter(todo => todo.completed).length;
   // Cantidad total de TODOs
@@ -83,7 +120,10 @@ function App() {
     saveTodos(newTodos);
   }
   return (
+    // Pasamos los valores de loading y error
     <AppUI
+    loading={loading}
+    error={error}
     totalTodos={totalTodos}
     completedTodos={completedTodos}
     searchValue={searchValue}
