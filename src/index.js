4c27aac3f112aec46c40c0529bb8214aaca74e97
pinajmr <pinajoma0@gmail.com>
2022-07-07 09:59:14 -0400

    add custom hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/index.js';
 
 // el método createRoot de ReactDom, en el que le pasamos el 
 //contenedor, guardamos este contenedor en una variable y luego ejecutamos el método 
